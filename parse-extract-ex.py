6c7aa4f3ed0f9624f9ce99643190f83783ff365b
frey-norden <frey.norden@icloud.com>
2020-04-15 18:20:16 +0300

    first look at this parse and extract pattern

---
+++ parse-extract-ex.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# File: parse-extract-ex.py
+
+def parse_extract():
+    data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+    print('This is the data', data)
+    atpos = data.find('@')
+    print('In what position is the @ symbol')
+    print(atpos)
+
+    sppos = data.find(' ',atpos)
+    print(sppos)
+
+    host = data[atpos+1 : sppos]
+    print(host)
+
+def main():
+    parse_extract()
+
+if __name__ == "__main__":
+    main()
