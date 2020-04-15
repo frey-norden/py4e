286da9be9eda99e89dfadaea014872dbf931cf83
frey-norden <frey.norden@icloud.com>
2020-04-15 10:44:02 +0300

    first commit

---
+++ strcatfunc.py
@@ -0,0 +1,14 @@
+# File: strcatfunc.py
+
+
+def dumbcatfunc():
+  a = 'Stickin '
+  b = a + 'Stings '
+  c = b + 'Together'
+  print(a,b,c)
+
+def main():
+  dumbcatfunc()
+
+if __name__ == "__main__":
+  main()
