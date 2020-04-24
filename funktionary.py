95523598f00da2e2466469cb1e0cfa027f75fead
frey-norden <frey.norden@icloud.com>
2020-04-25 01:58:50 +0300

    funkin with python

---
+++ funktionary.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# File: funktionary.py
+
+ccc = dict()
+ccc['pfunk']=7
+ccc['rickjames']=6
+print(ccc)
+print("ccc['pfunk'] += 3")
+ccc['pfunk'] += 3
+print('print(ccc)')
+print(ccc)
