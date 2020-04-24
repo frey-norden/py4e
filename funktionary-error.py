306d56d9dae242279dcad1032ede5dd74170884b
frey-norden <frey.norden@icloud.com>
2020-04-25 02:14:33 +0300

    funk error

---
+++ funktionary-error.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# File: funktionary-error.py
+
+print('This code will generate a funk error. \nIt fails to funk properly.')
+print('funk = dict()')
+funk = dict()
+print("funk['rickjames']")
+funk['rickjames']
