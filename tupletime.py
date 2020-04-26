8a1cc7df337ff36123cbd760a84f3a81cba02e98
frey-norden <frey.norden@icloud.com>
2020-04-27 01:39:57 +0300

    tuple examples

--- tupletime.py
+++ tupletime.py
@@ -21,3 +21,28 @@ print("fri = ('Smokie', 'Craig') > ('Deebo', 'Big Perm')")
 print("print(fri)")
 fri = ('Smokie', 'Craig') > ('Deebo', 'Big Perm')
 print(fri)
+
+# sorting lists of tuples
+# we can take advantage of the python's ability to sort
+# a list of tuples to get a sorted version of a dictionary
+# First we sort the dictionary by the key using the items() method
+# and sorted() function
+print('\n')
+print("d = {'a':10, 'b':1, 'c':22}")
+print("print(d.items())")
+d = {'a':10, 'b':1, 'c':22}
+print(d.items())
+print('\n')
+print("sorted(d.items())")
+print(sorted(d.items()))
+# We can do this even more directly using the built-in function
+# sorted that takes a sequence as a parameter and returns a
+# sorted sequence
+print('\n')
+print("t = sorted(d.items())")
+t = sorted(d.items())
+print(t)
+print("for k,v in sorted(d.items()):")
+print("    print(k, v)")
+for k,v in sorted(d.items()):
+    print(k, v)
