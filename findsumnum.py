b00aa2dfb3f8531117e8215f225f490e469c20fb
frey-norden <frey.norden@icloud.com>
2020-05-03 16:46:08 +0300

    commented out debug code

--- findsumnum.py
+++ findsumnum.py
@@ -13,17 +13,17 @@ for line in fh:
     line = line.rstrip()
     look = re.findall('[0-9]+', line)
     if len(look) == 0: continue
-    print(look)
+    #print(look)
     for i in range(0,len(look)):
         l = int(look[i])
         numlist.append(l)
 
 
-print(numlist)
+#print(numlist)
 
 for n in range(0,len(numlist)):
     numlist[n] = int(numlist[n])
     sum += numlist[n]
-    print(sum,n)
+    #print(sum,n)
 
 print(sum)
